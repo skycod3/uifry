ca11374e17848a017db141ff671dbe984185460c
Jean Medeiros <jeamcrv@hotmail.com>
2023-03-11 20:48:54 -0300

    Add ejs plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+
 export default defineConfig({
   resolve: {
     alias: {
       "@vendors": resolve(__dirname, "node_modules"),
     },
   },
+  plugins: [
+    ViteEjsPlugin(
+      {
+        title: "Uifry - Static Landing Page",
+      },
+      {
+        ejs: () => ({
+          filename: "src/includes/*.ejs",
+        }),
+      }
+    ),
+  ],
 });
